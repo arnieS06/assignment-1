d39daa4a27251be79b593617bdf48f9d33e49bce
arnieS06 <sharma.arnav2016@gmail.com>
2024-09-04 16:53:57 +0000

    added comments

--- UNTITLED 1.cpp
+++ UNTITLED 1.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
 
-    int myInt = 3;
-    int anotherInt = 7;
+    int myInt = 3; // first number defined
+    int anotherInt = 7; // second number defined
     
-    int holder = myInt;
+    int holder = myInt; // substitute variable to hold second number
     
-    myInt = anotherInt;
-    anotherInt = holder;
+    myInt = anotherInt; // first number value now equals second number
+    anotherInt = holder; // second number value now equals the first number
 
     return 0;
 
